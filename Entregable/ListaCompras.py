de9ea025a662edff28749305452562788008d9b0
marcos626 <marcossantillan4@gmail.com>
2023-07-15 23:24:05 -0300

    En esta versión lo envié al profe. Versión 2.0

--- Entregable/ListaCompras.py
+++ Entregable/ListaCompras.py
@@ -11,12 +11,13 @@ segundo artículo recién aparecía el que agregué primero.
 """
 import os
 import pandas as pd
-from moduloSantillan import ListaCompras, Multiplataforma
+from colorama import Fore, init
+init(autoreset=True)
+from moduloSantillan import ListaCompras
+from moduloSantillan import Multiplataforma as mf
 op1=int()
 op2=str()
 lc = ListaCompras(op1,op2) #instancio la clase ListaCompras
-mf = Multiplataforma()
-mf.clear_screen()
 
 while op1!=4: # Bucle de selección de archivos
     try:
@@ -27,36 +28,40 @@ while op1!=4: # Bucle de selección de archivos
             break
         elif op1==2: #Crear lista nueva
             fileSelected = lc.CrearArchivo()
+            input('El archivo se creó satisfactoriamente. Presione Enter para continuar')
             break
-            input('El archivo se creó satisfactoriamente. Presione cualquier tecla para continuar')
         elif op1==3: #Eliminar lista
             fileSelected=lc.AbrirArchivo()
             os.remove(fileSelected)
-            input('El archivo se borró satisfactoriamente. Presione cualquier tecla para continuar')
+            input('El archivo se borró satisfactoriamente. Presione Enter para continuar')
             break
         elif op1==4:
             print('Salir')
         else: 
-            input('Eligió una opción inexistente. Presione cualquier tecla para volver al menú de selección de listas de compras')
+            input(Fore.LIGHTRED_EX + 'Eligió una opción inexistente. Presione Enter para volver al menú de selección de listas de compras' + Fore.RESET)
     except ValueError:
-            print('ValueError: Se esperaba un entero, ingresó un caracter o un float?')
-            input('Presione cualquier tecla para continuar')
-    
+            input(Fore.LIGHTRED_EX + 'ValueError: Se esperaba un entero, ingresó un caracter o un float? Presione Enter para continuar' + Fore.RESET)
+                
 """----------- Menu Edición de listas de compras ----------"""
 
 while op1!=4:
     mf.clear_screen()
     op1=lc.menu_ABM_listas()
     if op1==1:   #Agregar artículos
-        print(f'el archivo elegido es {fileSelected}')
+        mf.clear_screen()
+        print(Fore.GREEN + f'el archivo elegido es {fileSelected}')
+        print("")
         df=pd.read_csv(fileSelected)
         print(df)
-        cant=input('ingrese la cantidad necesaria: ')
+        cant=int(input('ingrese la cantidad necesaria: '))
+        while cant<=0:
+            print('Error: La cantidad no puede ser menor o igual a cero')
+            cant=int(input('ingrese la cantidad necesaria: '))
         articulo=input('Escriba el artículo que desea agregar a la lista: ')
         rubro=input('ingrese el rubro: ')
         #posibles rubros: lacteos, limpieza, carnicería, verdulería, panificados
         with open(fileSelected,'a') as milista: #https://www.digitalocean.com/community/tutorials/how-to-handle-plain-text-files-in-python-3
-            milista.write(cant + "," + articulo + "," + rubro + "\n")
+            milista.write(str(cant) + "," + articulo + "," + rubro + "\n")
               
     elif op1==2: #Eliminar artículos
         mf.clear_screen()
@@ -68,23 +73,29 @@ while op1!=4:
             print(df)
             df.to_csv(fileSelected, index=False)
         except KeyError:
-            print('Intenta borrar un ítem inexistente')
-        input('presione cualquier tecla para volver al menu de edición de la lista')
+            print(Fore.RED + 'Intenta borrar un ítem inexistente')
+        input('presione Enter para volver al menu de edición de la lista')
 
     elif op1==3: #mostrar lista
         mf.clear_screen()
         df=pd.read_csv(fileSelected)
+        print(Fore.LIGHTBLUE_EX + 'Lista sin ordenar' + Fore.RESET)
         print(df)
+        print('')
+        byRubro=df.sort_values(['rubro','articulo'])
+        byRubro.head()
+        print(Fore.LIGHTCYAN_EX + 'Lista ordenada por rubro y articulo' + Fore.RESET)
+        print(byRubro)
         #agrupar por rubro para mostrar ordenados
         
         op2=input("Desea seguir editando esta lista?: [S/N]")
         if str.lower(op2) == 's':
             op1=1 #para continuar en este menu
-            os.system("clear")
+            mf.clear_screen()
         elif str.lower(op2) == "n":
             break
         else:
             print("no es una opcion valida")
             break
 print("")
-print("Lista de compras terminada")
+print(Fore.LIGHTBLUE_EX + f"Lista de compras terminada")
--- Entregable/moduloSantillan.py
+++ Entregable/moduloSantillan.py
@@ -1,34 +1,34 @@
 """
 
-módulo contenedor de la clase Lista_compras y sus métodos para manejo de archivos csv.
+módulo contenedor de la clase Lista_compras y Multiplataforma.
 """
 
 import os, glob
 import pandas as pd
 import platform
 from os.path import normpath
+from colorama import Fore, init
+init(autoreset=True)
 
 class Multiplataforma:
     """ Clase contenedora de métodos para correr el código en distintos sistemas operativos"""
-    def __init__(self):
-        pass
-    def clear_screen(self):
+    def clear_screen():
         """ Método multiplataforma para limpiar pantalla
         platform.system returns the system/OS name, such as 'Linux', 'Darwin', 'Java', 'Windows'. 
         An empty string is returned if the value cannot be determined.
         """
         os.system('cls' if platform.system == 'Windows' else 'clear')
         return
-    def normalizarRuta(self,ruta):
+    
+    def normalizarRuta(ruta):
         """Método que, en función del sistema operativo
         acondiciona la dirección, por ejemplo, invierte las barras para 
         que sea compatible con Windows"""
-        self.ruta=ruta
-        normpath(self.ruta)
-        return self.ruta
+        normpath(ruta)
+        return ruta
 
 class ListaCompras:
-    """ Esta clase tiene métodos para manejo de archivos """
+    """ Esta clase tiene métodos para manejo de archivos .csv"""
     
     def __init__(self, op1, op2):
         self.op1=op1
@@ -36,56 +36,69 @@ class ListaCompras:
          
     def menuSeleccionArchivos(self):
         """ Muestra un menú con 4 opciones: abrir una lista, crear lista nueva, eliminar lista o salir"""
-        print('Menu de selección de listas de compras')
+        print(Fore.LIGHTBLUE_EX + f'Menu de selección de listas de compras')
+        print(Fore.LIGHTBLUE_EX + f'--------------------------------------' + Fore.RESET)
         print("1 Abrir lista existente")
         print("2 Crear lista nueva")
-        print("3 Eliminar una lista")
+        print("3 Eliminar lista")
         print("4 Salir")
+        print("")
         op=int(input('Seleccione una opción: '))
         return op
 
     def menu_ABM_listas(self):
         """ Muestra un menú con 4 opciones y devuelve un int con  la opción elegida"""
-        print("******Lista de compras******")
-        print("1)- Agregar articulos      *")
-        print("2)- Eliminar articulos     *")
-        print("3)- Mostrar lista         *")
-        print("4)- Salir                  *")
-        print("****************************")
-        op=int(input("Seleccione una opción: "))
-        return op
-
+        try:
+            print(Fore.LIGHTMAGENTA_EX + f"******Lista de compras******")
+            print("")
+            print("1)- Agregar articulos      *")
+            print("2)- Eliminar articulos     *")
+            print("3)- Mostrar lista          *")
+            print("4)- Salir                  *")
+            print("")
+            op=int(input("Seleccione una opción: "))
+            return op
+        except IndexError:
+            input(Fore.LIGHTRED_EX + 'IndexError: Eligió un archivo inexistente. Presione Enter para continuar' + Fore.RESET) 
+        except ValueError:
+            input(Fore.LIGHTRED_EX + 'ValueError: Debe introducir un índice, no el nombre del archivo. Presione Enter para continuar' + Fore.RESET)
+        
     def AbrirArchivo(self):
         """ Muestra una lista con todos los archivos .csv y permite elegir alguno de ellos"""
         try:
-            print('Estoy dentro de la Función AbrirArchivo')
-            os.chdir(os.getcwd()+'/Entregable')
+            Multiplataforma.clear_screen()
+            print(Fore.LIGHTBLUE_EX + 'Listas de compras disponibles' + Fore.RESET)
+            print('')
+            ruta = os.getcwd()+'/Entregable'
+            Multiplataforma.normalizarRuta(ruta)
+            os.chdir(ruta)
             cwd=os.getcwd()
-            print(f'cwd es {cwd}')
             csvfiles=glob.glob(os.path.join(cwd, '*.csv')) #csvfiles es una lista con todos los archivos .csv encontrados
             i=0
             for csvfile in csvfiles:
                 print(f'{i} - {csvfile}')
                 i+=1
+            print('')
             op=int(input('Seleccione un archivo: '))
             fileSelected=csvfiles[op]
             os.chdir('..')    
             return fileSelected
         except IndexError:
-            print('IndexError: Eligió un archivo inexistente')
+            input(Fore.LIGHTRED_EX + 'IndexError: Eligió un archivo inexistente. Presione Enter para volver al menu de selección de archivos' + Fore.RESET)
         except ValueError:
-            print('ValueError: Debe introducir un índice, no el nombre del archivo')
-        input('Presione cualquier tecla para volver al menu de selección de archivo')
-        os.chdir('..')
+            input(Fore.LIGHTRED_EX + 'ValueError: Se esperaba un tipo de dato entero. Presione Enter para volver al menu de selección de archivos' + Fore.RESET)
+        os.chdir('..') #Vuelvo a la carpeta original
         ListaCompras.AbrirArchivo(self)
-
-
 
     def CrearArchivo(self):
         """ Crea un archivo .csv con encabezado: cantidad, producto y rubro."""
+        Multiplataforma.clear_screen()
+        flag=bool()
         print('Dentro de la función CrearArchivo')
         print(f'Estoy parado en la carpeta: {os.getcwd()}')
-        os.chdir(os.getcwd()+'/Entregable')
+        ruta = os.getcwd()+'/Entregable'
+        Multiplataforma.normalizarRuta(ruta)
+        os.chdir(ruta)
         cwd=os.getcwd()
         print(f'Luego del chdir, ahora estoy en: {os.getcwd()}')
         csvfiles=glob.glob(os.path.join(cwd, '*.csv')) #csvfiles es una lista con todos los archivos .csv encontrados
@@ -97,14 +110,20 @@ class ListaCompras:
         nombre=input('ingrese el nombre de la nueva lista: ')
         csvfiles=glob.glob(os.path.join(cwd, '*.csv')) #lista con todos los archivos .csv
         newFile=(f'{cwd}/{nombre}.csv')
+        Multiplataforma.normalizarRuta(newFile) #invertir las barras si está en Windows
         print(f'El archivo nuevo es: {newFile}')
         for csvfile in csvfiles: #estructura de control For-else para encontrar un archivo
             if csvfile == (newFile):
-                print('Ya existe un archivo con ese nombre, pruebe con otro nombre')
+                input(Fore.LIGHTRED_EX + 'Ya existe un archivo con ese nombre, pruebe con otro nombre. Presione Enter para continuar' + Fore.RESET)
                 os.chdir('..') #vuelvo a la carpeta original
-                ListaCompras.CrearArchivo(self)
+                newFile = ListaCompras.CrearArchivo(self)
+                if flag==True:
+                    flag=False
+                    break
         else: #Si no encontró ninguno con el mismo nombre, continua con el else
             df.to_csv(newFile, index=False)
+            print(df)
             os.chdir('..') #vuelvo a la carpeta original
-            print(f'Vuelvo a la carpeta original {os.getcwd()}') #me fijo en que carpeta estoy
+            #print(f'Vuelvo a la carpeta original {os.getcwd()}') #me fijo en que carpeta estoy
+            flag=True #bandera en uno para indicar que se creó un nuevo archivo
             return newFile
