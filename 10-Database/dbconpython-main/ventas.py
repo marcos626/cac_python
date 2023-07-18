94a04f793ccf124ef895fa305fd2423f6f6fba1e
marcos626 <marcossantillan4@gmail.com>
2023-07-18 16:32:08 -0300

    Ventas del profe Serbluk

---
+++ 10-Database/dbconpython-main/ventas.py
@@ -0,0 +1,197 @@
+#importo el conector
+import mysql.connector
+
+def conectar():
+    #intentar conectar a la base de datos
+    try:
+        coneccion = mysql.connector.connect(host="db4free.net", 
+            user='pruebascodo', password='746650ac', database='pruebascodo')
+        return coneccion   
+    except mysql.connector.Error as error:
+        print("Error al conectar a la base de datos: {}".format(error))
+        return None
+def desconectar(coneccion):
+    #desconectar de la base de datos
+    coneccion.close()
+    print("La coneccion a la base de datos ha finalizado")
+def ejecutar_consulta(coneccion, consulta,comiteo=True):
+    #ejecutar una consulta
+    try:
+        cursor = coneccion.cursor()
+        cursor.execute(consulta)
+        if comiteo==True:
+            coneccion.commit()
+        return cursor
+    except mysql.connector.Error as error:
+        print("Error al ejecutar consulta: {}".format(error))
+        return None    
+def menu_productos():
+    #mostrar el menu de productos
+    print("Menu de productos")
+    print("1. Listar productos")
+    print("2. Agregar producto")
+    print("3. Editar producto")
+    print("4. Eliminar producto")
+    print("5. Salir")
+    opcion = input("Ingrese una opcion: ")
+    return opcion
+def menu_ventas():
+    #mostrar el menu de ventas
+    print("Menu de ventas")
+    print("1. Listar ventas")
+    print("2. Agregar venta")
+    print("3. Editar venta")
+    print("4. Eliminar venta")
+    print("5. Salir")
+    opcion = input("Ingrese una opcion: ")
+    return opcion
+def menu_principal():
+    #mostrar el menu principal
+    print("Menu principal")
+    print("1. Productos")
+    print("2. Ventas")
+    print("3. Salir")
+    opcion = input("Ingrese una opcion: ")
+    return opcion
+while True:
+    menu_princ = menu_principal()
+    if menu_princ == "1":
+        menu_prod=menu_productos()
+        if menu_prod=="1":
+            #listar productos
+            coneccion = conectar()
+            if coneccion:
+                consulta = "SELECT * FROM pruebascodo.productos"
+                micursor=ejecutar_consulta(coneccion, consulta, False)
+                if micursor:
+                    for fila in micursor:
+                        print(fila)
+                desconectar(coneccion)
+        elif menu_prod=="2":
+            #agregar producto
+            coneccion = conectar()
+            if coneccion:
+                nombre = input("Ingrese el nombre del producto: ")
+                precio = float(input("Ingrese el precio del producto: "))
+                consulta = "INSERT INTO productos(descripcion, precio) VALUES ('{}', {})".format(nombre, precio)
+                cursor = ejecutar_consulta(coneccion, consulta)
+                if cursor:
+                    print("Producto agregado correctamente")
+                desconectar(coneccion)
+        elif menu_prod=="3":
+            #editar producto
+            coneccion = conectar()
+            if coneccion:
+                id_producto = int(input("Ingrese el id del producto a editar: "))
+                nombre = input("Ingrese el nuevo nombre del producto: ")
+                precio = float(input("Ingrese el nuevo precio del producto: "))
+                consulta = "UPDATE productos SET descripcion='{}', precio={} WHERE id_p={}".format(nombre, precio, id_producto)
+                cursor = ejecutar_consulta(coneccion, consulta)
+                if cursor:
+                    print("Producto editado correctamente")
+                desconectar(coneccion)
+        elif menu_prod=="4":
+            #eliminar producto
+            coneccion = conectar()
+            if coneccion:
+                id_producto = int(input("Ingrese el id del producto a eliminar: "))
+                consulta = "DELETE FROM productos WHERE id_p={}".format(id_producto)
+                #habia agregado la linea del print
+                cursor = ejecutar_consulta(coneccion, consulta)
+                if cursor:
+                    print("Producto eliminado correctamente")
+                desconectar(coneccion)
+        elif menu_prod=="5":
+            #salir
+            break
+        else:
+            print("Opcion incorrecta")
+    elif menu_princ == "2":
+        #menu ventas
+        menu_vent=menu_ventas()
+        if menu_vent=="1":
+            #listar ventas
+            coneccion = conectar()
+            if coneccion:
+                consulta = "SELECT ventas.nFactura, ventas.fecha, ventas.cliente,detVentas.id_p, detVentas.cantidad, detVentas.importe FROM ventas, detVentas WHERE ventas.nFactura=detVentas.nFactura"
+                cursor = ejecutar_consulta(coneccion, consulta,False)
+                if cursor:
+                    for fila in cursor:
+                        print(fila)
+                desconectar(coneccion)
+        elif menu_vent=="2":
+            #agregar venta
+            coneccion = conectar()
+            if coneccion:
+                fecha=input("Ingrese la fecha de la venta: ")
+                cliente=input("Ingrese el cliente: ")
+                nfactura=int(input("Ingrese el numero de factura: "))
+                #monto=float(input("Ingrese el monto: "))
+                consulta = "INSERT INTO ventas(nFactura, fecha, cliente) VALUES ({}, '{}', '{}')".format(nfactura, fecha, cliente)
+                cursor = ejecutar_consulta(coneccion, consulta)
+                coneccion.commit()
+                if cursor:
+                    mibandera=True
+                id_producto = int(input("Ingrese el id del producto: "))
+                cantidad = int(input("Ingrese la cantidad: "))
+                #busco el precio del producto en la tabla productos
+                consulta = "SELECT precio FROM productos WHERE id_p={}".format(id_producto)
+                cursor = ejecutar_consulta(coneccion, consulta, False)
+                if cursor:
+                    for fila in cursor:
+                        precio=fila[0]
+                #calculo el importe
+                importe=precio*cantidad
+                #agrego el detalle de la venta
+                consulta = "INSERT INTO detVentas(nFactura, id_p, cantidad, importe) VALUES ({}, {}, {}, {})".format(nfactura, id_producto, cantidad, importe)
+                cursor = ejecutar_consulta(coneccion, consulta)
+                coneccion.commit()
+                if cursor:
+                    print("Detalle de venta agregado correctamente")
+                #actualizo el monto de la venta
+                consulta = "UPDATE ventas SET monto=monto+{} WHERE nFactura={}".format(importe, nfactura)
+                cursor = ejecutar_consulta(coneccion, consulta)
+                coneccion.commit()
+                if cursor:
+                    print("Venta agregada correctamente")
+                desconectar(coneccion)
+        elif menu_vent=="3": #falta seguir desde aca
+            #editar venta
+            coneccion = conectar()
+            if coneccion:
+                id_venta = int(input("Ingrese el id de la venta a editar: "))
+                id_producto = int(input("Ingrese el nuevo id del producto: "))
+                cantidad = int(input("Ingrese la nueva cantidad: "))
+                consulta = "UPDATE ventas SET id_producto={}, cantidad={} WHERE id={}".format(id_producto, cantidad, id_venta)
+                cursor = ejecutar_consulta(coneccion, consulta)
+                if cursor:
+                    print("Venta editada correctamente")
+                desconectar(coneccion)
+        elif menu_vent=="4":
+            #eliminar venta
+            coneccion = conectar()
+            if coneccion:
+                id_venta = int(input("Ingrese el id de la venta a eliminar: "))
+                consulta = "DELETE FROM ventas WHERE id={}".format(id_venta)
+                cursor = ejecutar_consulta(coneccion, consulta)
+                if cursor:
+                    print("Venta eliminada correctamente")
+                desconectar(coneccion)
+        elif menu_vent=="5":
+            #salir
+            break
+        else:
+            print("Opcion incorrecta")
+    elif menu_princ == "3":
+        #salir
+        break
+    else:
+        print("Opcion incorrecta")
+
+        
+        
+        
+    
+    
+    
+    
